ab33f6b6feaa3c4a0d85dbf1b9a37b88342d343d
Sourabh7singh <souravhsingh2002@gmail.com>
2025-03-21 21:51:25 +0530

    added all files

---
+++ dukan-near-me/src/middleware.js
@@ -0,0 +1,27 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized: ({ token, req }) => {
+      const path = req.nextUrl.pathname;
+
+      if (!token) return false;
+
+      if (token.role === "admin") return true;
+
+      return token.allowedRoutes?.includes(path)
+    },
+  },
+});
+
+export const config = {
+  matcher: [
+    "/",
+    "/dashboard/:path*",
+    "/admin/:path*",
+    "/content/:path*",
+  ],
+};
