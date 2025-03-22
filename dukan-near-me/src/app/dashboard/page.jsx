b543ba3739ff7c24ddd48c3524be9acf1c2f798c
Rhydam Kapoor <rhydamkapoor@gmail.com>
2025-03-22 15:45:38 +0530

    demo merging process...

--- dukan-near-me/src/app/dashboard/page.jsx
+++ dukan-near-me/src/app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import LogoutButton from "@/components/LogoutButton";
+import LogoutButton from "@/app/components/LogoutButton";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
