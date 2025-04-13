718264e273e44446bdb262285f0e53561be1132a
Sourabh7singh <souravhsingh2002@gmail.com>
2025-04-13 17:47:02 +0530

    updated next.config for allowed image patterns

--- dukan-near-me/next.config.mjs
+++ dukan-near-me/next.config.mjs
@@ -6,7 +6,13 @@ const nextConfig = {
         },
     },
     images: {
-      domains: ['res.cloudinary.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          pathname: '/**',
+        },
+      ],
     },
 };
 
