28b3062c185247f2309db5c6d955590894290820
Sourabh7singh <souravhsingh2002@gmail.com>
2025-05-01 22:50:35 +0530

    added changes for getting user based on a username

--- dukan-near-me/src/app/api/users/route.js
+++ dukan-near-me/src/app/api/users/route.js
@@ -1,12 +1,37 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/db";
 
-export const GET = async () => {
+export const GET = async (request) => {
   try {
-    const users = await prisma.user.findMany({ where: { role: "USER" } });
+    const { searchParams } = new URL(request.url);
+    const search = searchParams.get("search")?.trim();
+
+    let users;
+
+    if (search) {
+      users = await prisma.user.findMany({
+        where: {
+          role: "USER",
+          username: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      });
+    } else {
+      users = await prisma.user.findMany({
+        where: {
+          role: "USER",
+        },
+      });
+    }
+
     return NextResponse.json({ data: users }, { status: 200 });
   } catch (error) {
-    console.log("error",error);
-    return NextResponse.json({ message: "Failed to fetch users", error }, { status: 500 });
+    console.error("Error fetching users:", error);
+    return NextResponse.json(
+      { message: "Failed to fetch users", error },
+      { status: 500 }
+    );
   }
 };
