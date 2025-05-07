7bf553511c8b074386d38d532564714c12ab06c6
@piyush.singh <piyushsinghchauhan5816@gmail.com>
2025-05-07 20:31:26 +0530

    qr in token generation

--- dukan-near-me/src/app/scan-qr/page.jsx
+++ dukan-near-me/src/app/scan-qr/page.jsx
@@ -5,7 +5,7 @@ import QrScanner from '../components/qr-scanner/QrScanner';
 
 function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 w-full">
       <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-lg">
         <h1 className="text-3xl font-semibold text-center text-gray-700 mb-6">QR Code Scanner</h1>
         <div className="text-center text-gray-500 mb-4">
