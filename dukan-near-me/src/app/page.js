33de596b65d2a15311e426e0c0d4e6dcda7f55b0
Sourabh7singh <souravhsingh2002@gmail.com>
2025-05-14 23:06:55 +0530

    added main page on / route

--- dukan-near-me/src/app/page.js
+++ dukan-near-me/src/app/page.js
@@ -1,28 +1,33 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import PaymentHistory from "./components/chat/PaymentHistory";
+import Image from "next/image";
+import RolesCard from "@/app/components/GetStarted/RolesCard";
+import Link from "next/link";
 
-export default function Home() {
+export default function GetStarted() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[var(--font-rubik)]">
-      <Dialog>
-        <DialogTrigger asChild>
-          <span>+</span>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[500px] h-4/5 rounded-xl border-none flex flex-col bg-[#F5FAFC] overflow-auto dialogScroll">
-          <DialogHeader>
-            <div className="hidden">
-              <DialogTitle></DialogTitle>
-              <DialogDescription></DialogDescription>
-            </div>
-            <div className="flex justify-center items-center font-[family-name:var(--font-plusJakarta)]">
-              <button className="bg-teal-700 p-3 w-3/4 outline-none rounded-xl text-white text-sm">Your all time transaction history</button>
-            </div>
-          </DialogHeader>
-          <div className="flex flex-col">
-            <PaymentHistory />
+    <main className='h-screen overflow-hidden bg-gradient-to-br from-[#e7f0ec] via-[#aabec2] to-[#005d6e]'>
+      <header className='flex justify-between p-5'>
+        <div className="w-3/4 flex items-center gap-x-2">
+          <div className="relative w-10 h-10">
+            <Image src="/nearbuydukan-Logo/Logo.svg" alt="nearbuydukan" fill sizes='50px' priority />
           </div>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+          <span className="font-semibold text-sm uppercase">nearbuydukan</span>
+        </div>
+        <div className="flex items-center whitespace-nowrap text-sm gap-x-2 md:gap-x-5 text-slate-700">
+            {/* <button className="cursor-pointer">Need help?</button> */}
+            {/* <button className="cursor-pointer">Contact us</button> */}
+            <Link href="/aboutus#help" className="text-[10px] md:text-sm cursor-pointer">Need help?</Link>
+            <Link href="mailto:contact@nearbuydukaan.com" className="text-[10px] md:text-sm cursor-pointer">Contact us</Link>
+
+        </div>
+      </header>
+      <section className='flex justify-center items-center h-full'>
+        <div className="flex flex-col items-center w-4/5 md:w-2/6 h-3/4 md:h-full">
+            <div className="flex">
+                <h1 className='text-2xl md:text-3xl font-bold text-slate-800'>Start with us now</h1>
+            </div>
+            <RolesCard />
+        </div>
+      </section>
+    </main>
+  )
 }
