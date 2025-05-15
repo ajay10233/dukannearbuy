a4470224936f6c2c6390d98b82cac8ba8a7f0f6d
Sourabh7singh <souravhsingh2002@gmail.com>
2025-05-16 00:17:07 +0530

    removed localhost

--- dukan-near-me/src/lib/socket.js
+++ dukan-near-me/src/lib/socket.js
@@ -4,8 +4,9 @@ import { io } from "socket.io-client";
 let socket;
 
 export const initiateSocket = () => {
+  
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002",{transports: ["websocket"]});
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL,{transports: ["websocket"]});
   }
   console.log(socket);
   return socket;
