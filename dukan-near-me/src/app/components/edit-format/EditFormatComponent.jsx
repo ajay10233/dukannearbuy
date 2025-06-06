5338de6ec4ee9b6ba297d8d131e379dc7286755f
Sourabh7singh <souravhsingh2002@gmail.com>
2025-06-06 22:40:04 +0530

    added status to bill generated

--- dukan-near-me/src/app/components/edit-format/EditFormatComponent.jsx
+++ dukan-near-me/src/app/components/edit-format/EditFormatComponent.jsx
@@ -255,7 +255,7 @@ export default function EditFormatComponent() {
         }
 
         toast.success('Bill generated successfully!');
-        socket?.emit("sendNotification",{toUserId:userId,message:"You have a new bill generated",fromUserId:session?.user?.id,status:"generated"});
+        socket?.emit("sendNotification",{toUserId:userId,message:"You have a new bill generated",fromUserId:session?.user?.id,status:"bill-generated"});
         console.log('Generated bill:', data);
 
     } catch (error) {
