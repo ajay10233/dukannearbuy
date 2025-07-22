e4a1aadabd1ecb97a26c1e0f5d250100ad9772be
Garima Rawat <ms.garimarawat@gmail.com>
2025-07-22 23:10:02 +0530

    fixed top seller card issue

--- dukan-near-me/src/app/components/userProfile/TopSeller/TopSeller.jsx
+++ dukan-near-me/src/app/components/userProfile/TopSeller/TopSeller.jsx
@@ -27,6 +27,7 @@ export default function TopSeller() {
   useEffect(() => {
     if (emblaApi) emblaApi.on("select", onSelect);
   }, [emblaApi, onSelect]);
+  
 
   const fetchTopProfiles = async (latitude, longitude) => {
     try {
@@ -144,7 +145,7 @@ export default function TopSeller() {
                     className="px-2 flex-[0_0_100%] sm:flex-[0_0_50%] md:flex-[0_0_33.3333%] xl:flex-[0_0_25%]"
                   >
                     <Link href={`/partnerProfile/${seller?.user?.id}`} className="block cursor-pointer">
-                      <div className="bg-white rounded-xl shadow-lg overflow-hidden flex flex-col justify-center relative border border-amber-300">
+                      <div className="bg-white w-full h-[310px] rounded-xl shadow-lg overflow-hidden flex flex-col justify-center relative border border-amber-300">
                         <div className="relative w-full h-52 border-b border-amber-300">
                           {/* <Image
                             src={seller?.user?.photos?.[0] || "/default-img.jpg"}
@@ -164,6 +165,7 @@ export default function TopSeller() {
                             alt={seller?.user?.firmName || "Default Image"}
                             fill
                             className="object-cover"
+                            sizes="(max-width: 768px) 100vw, 25vw"
                             priority
                           />
 
